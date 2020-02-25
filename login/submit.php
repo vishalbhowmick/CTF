bdf142f0d8fff0c37973389daa823442337c2c16
Vishal Bhowmick <46847162+vishalbhowmick@users.noreply.github.com>
2020-02-25 16:28:04 +0530

    Add files via upload

---
+++ login/submit.php
@@ -0,0 +1,5 @@
+<?php if(($_POST["username"]==="cooldudeugtfe") && ($_POST["password"]==="afuy5#*gF^683e29_)"))
+{echo "username and password is correct";}
+else
+{echo "username and password is incorrect";}
+?>
